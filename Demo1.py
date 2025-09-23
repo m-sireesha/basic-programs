029649b0fe7b879954b6d1f807bb858b7eb55629
Seshu Ranjit Kumar Gunturu <seshuranjitkumargunturu@Seshus-MBP.lan>
2025-09-23 15:46:06 -0500

    Add Demo1.py

---
+++ Demo1.py
@@ -0,0 +1,15 @@
+print("hello"+"world")
+print("hello","world")
+a,b,c=10,20,30
+print("values",a,b,c)
+print("values",a,b,c,sep=',')
+print("values",a,b,c,sep=':')
+print("hello ",end='')
+print(" welcome to",end='')
+print(" python")
+
+name="sireesha"
+salary=6000
+gf="funny"
+print("hello {0} your salary is {1} and yout friend {2} is waiting for you ".format(name,salary,gf))
+print("hello {x} your salary is {y} and yout friend {z} is waiting for you ".format(x=name,y=salary,z=gf))
